745a017eae9d61b74386f2188f2ccbeb642bdec8
anton_malashenko <anton.mal.slb@gmail.com>
2022-05-27 10:41:13 +0300

    impl triangle

--- triangle.go
+++ triangle.go
@@ -1,7 +1,17 @@
 package golang_united_school_homework
 
+import "math"
+
 // Triangle must satisfy to Shape interface
 type Triangle struct {
 	Shape
 	Side float64
+}
+
+func (t Triangle) CalcPerimeter() float64 {
+	return t.Side * 3
+}
+
+func (t Triangle) CalcArea() float64 {
+	return (t.Side * t.Side * math.Sqrt(3)) / 4
 }
